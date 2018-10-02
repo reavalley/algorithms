958be8d4d78daba36b8ce89151a8810a00ee93cf
Russ Wicks <russ.wicks@digital.hmrc.gov.uk>
2018-10-02 14:42:45 +0100

    initial commit

---
+++ Algorithms/BubbleSort.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public class BubbleSort
+    {
+        public static IEnumerable<int> Sort(int[] listToSort)
+        {
+            int m = 0;
+            int k = 0;
+            int n = listToSort.Length - 1;
+
+
+            while (m < n)
+            {
+                while (k < n)
+                {
+                    if (listToSort[k] > listToSort[k+1])
+                    {
+                        int a = listToSort[k];
+                        int b = listToSort[k + 1];
+
+                        listToSort[k] = b;
+                        listToSort[k + 1] = a;
+                    }
+
+                    k++;
+                }
+
+                k = 0;
+                m++;
+            }
+
+            return listToSort;
+        }
+        
+        public IEnumerable<int> Sort1(int[] listToSort)
+        {
+            foreach (var val in listToSort)
+            {
+                for (var i = 0; i < listToSort.Length; i++)
+                {
+                    if (i + 1 > listToSort.Length - 1) continue;
+                
+                    var a = listToSort[i];
+                    var b = listToSort[i + 1];
+
+                    if (a <= b) continue;
+                
+                    listToSort[i] = b;
+                    listToSort[i + 1] = a;
+                }
+            }
+
+            return listToSort;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sorter = new BubbleSort();
+            
+            var result = BubbleSort.Sort(new int[]{29,27,3,11,18,28,13 });
+            
+            var output = string.Join(",", result.Select(p=>p.ToString()).ToArray());
+            
+            Console.WriteLine(output);
+        }
+    }
+}
